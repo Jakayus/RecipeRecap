e59132e5be9a42c9f5b6faf05374396bd103c743
Joel Sereno <jakayus123@gmail.com>
2023-11-14 22:06:54 -0700

    add Xcode project

---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Joel Sereno on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
